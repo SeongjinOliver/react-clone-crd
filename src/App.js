629f6ee5a7be3bb588dd06fe65eec22c71fc3d4f
SeongjinOliver <yoosngj@gmail.com>
2023-02-19 18:39:57 +0900

    Study CRUD from React

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import TodoList from "./component/TodoList";
+import Header from "./component/Header/Header";
+import {useState} from "react";
+
+const filters = ['all', 'active', 'completed'];
 
 function App() {
-    return (
-        <div>
-            <TodoList/>
-        </div>
-    );
+  const [filter, setFilter] = useState(filters[0]);
+  return (
+    <div>
+      <Header filters={filters}
+              filter={filter}
+              onFilterChange={setFilter}/>
+      <TodoList filter={filter}/>
+    </div>
+  );
 }
 
 export default App;
