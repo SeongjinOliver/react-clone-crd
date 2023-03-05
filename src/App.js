3d6f3429aaf1db3d57f069a29d3b5e93575fe32e
SeongjinOliver <yoosngj@gmail.com>
2023-03-05 14:48:24 +0900

    Study from React

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import './App.css';
 import TodoList from "./component/TodoList";
 import Header from "./component/Header/Header";
 import {useState} from "react";
+import styles from "./App.module.css"
 
 const filters = ['all', 'active', 'completed'];
 
 function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <div>
+    <>
       <Header filters={filters}
               filter={filter}
               onFilterChange={setFilter}/>
       <TodoList filter={filter}/>
-    </div>
+    </>
   );
 }
 
