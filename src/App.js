19f52bdd9148caac345567f97cc5c4c60a12884f
SeongjinOliver <yoosngj@gmail.com>
2023-03-07 01:19:01 +0900

    Study from React

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import TodoList from "./component/TodoList/TodoList";
 import Header from "./component/Header/Header";
 import {useState} from "react";
 import styles from "./App.module.css"
+import {DarkModeProvider} from "./context/DarkModeContext";
 
 const filters = ['all', 'active', 'completed'];
 
 function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <>
+    <DarkModeProvider>
       <Header filters={filters}
               filter={filter}
               onFilterChange={setFilter}/>
       <TodoList filter={filter}/>
-    </>
+    </DarkModeProvider>
   );
 }
 
